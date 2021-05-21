d035fbaad5f519fe91f529e2a0ed8a86663a48d1
Rostyk <rostishka1@gmail.com>
2021-05-21 07:23:14 +0300

    setup customers model

--- src/customers/models.py
+++ src/customers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Customers(models.Model):
+    name = models.CharField(max_length=120)
+    logo = models.ImageField(upload_to='customers', default='no_picture.png')
+
+    #string representation
+    def __str__(self):
+        return str(self.name)
+
+
