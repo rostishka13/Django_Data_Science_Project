880f2b2af5d777a7f6e714e7ab771617c8f58fa7
Rostyk <rostishka1@gmail.com>
2021-05-21 06:55:43 +0300

    setup static files, directory, static root, media root

--- src/reports_proj/settings.py
+++ src/reports_proj/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'products',
     'profiles',
     'users',
+    #forms
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -60,7 +62,7 @@ ROOT_URLCONF = 'reports_proj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ os.path.join(BASE_DIR, 'tempalates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +126,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
