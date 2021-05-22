979cc5ab755b1ae43173d04d6591bb21bb050ee5
Rostyk <rostishka1@gmail.com>
2021-05-22 20:14:06 +0300

    add data frame

--- src/sales/views.py
+++ src/sales/views.py
@@ -2,12 +2,23 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Sales
 from .forms import SalesSearchForm
+import pandas as pd
 # Create your views here.
 def home_view(request):
     form = SalesSearchForm(request.POST or None)
-    hello = 'hello from the view'
+    if request.method == 'POST':
+        date_from = request.POST.get('date_from')
+        date_to = request.POST.get('date_to')
+        chart_type = request.POST.get('chart_type')
+        qs = Sales.objects.filter(created__date=date_from)
+        df1 = pd.DataFrame(qs.values())
+        df2 = pd.DataFrame(qs.values_list())
+        print(df1)
+        print('#######')
+        # print(df2)
+
     context = {
-        'hello': hello,
+
         'form': form,
     }
     return render(request, 'sales/home.html',context )
