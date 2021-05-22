e24dc57d3b26827f9dc031dccc7a5a3815545c92
Rostyk <rostishka1@gmail.com>
2021-05-22 20:30:48 +0300

    rendering dataframe

--- src/sales/views.py
+++ src/sales/views.py
@@ -5,20 +5,24 @@ from .forms import SalesSearchForm
 import pandas as pd
 # Create your views here.
 def home_view(request):
+    sales_df = None
     form = SalesSearchForm(request.POST or None)
     if request.method == 'POST':
         date_from = request.POST.get('date_from')
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
-        qs = Sales.objects.filter(created__date=date_from)
-        df1 = pd.DataFrame(qs.values())
-        df2 = pd.DataFrame(qs.values_list())
-        print(df1)
-        print('#######')
-        # print(df2)
+        qs = Sales.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
+        if len(qs) > 0:
+            sales_df = pd.DataFrame(qs.values())
+
+            sales_df = sales_df.to_html()
+            # print(sales_df)
+        else:
+            print('No data')
+
 
     context = {
-
+        'sales_df':sales_df,
         'form': form,
     }
     return render(request, 'sales/home.html',context )
