05af5f86d901943d3832d6f892f70110200c3ac4
Rostyk <rostishka1@gmail.com>
2021-05-22 21:02:17 +0300

    rendering position dataframe

--- src/sales/views.py
+++ src/sales/views.py
@@ -6,17 +6,31 @@ import pandas as pd
 # Create your views here.
 def home_view(request):
     sales_df = None
+    position_df = None
     form = SalesSearchForm(request.POST or None)
     if request.method == 'POST':
         date_from = request.POST.get('date_from')
         date_to = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
-        qs = Sales.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
-        if len(qs) > 0:
-            sales_df = pd.DataFrame(qs.values())
+        sale_qs = Sales.objects.filter(created__date__lte=date_to, created__date__gte=date_from)
+        if len(sale_qs) > 0:
+            sales_df = pd.DataFrame(sale_qs.values())
+            positions_data = []
+            for sale in sale_qs:
+                for pos in sale.get_positions():
+                    obj = {
+                        'position_id': pos.id,
+                        'product': pos.product.name,
+                        'quantity': pos.quantity,
+                        'price': pos.price,
+                        }
+                    positions_data.append(obj)
 
+            position_df = pd.DataFrame(positions_data)
+
+            print(position_df)
             sales_df = sales_df.to_html()
-            # print(sales_df)
+            position_df = position_df.to_html()
         else:
             print('No data')
 
@@ -24,8 +38,9 @@ def home_view(request):
     context = {
         'sales_df':sales_df,
         'form': form,
+        'position_df': position_df
     }
-    return render(request, 'sales/home.html',context )
+    return render(request, 'sales/home.html', context)
 
 
 class SaleListView(ListView):
