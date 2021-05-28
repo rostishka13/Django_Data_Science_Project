b62f0d40426507424b958c60eba0106270b9b61d
Rostyk <rostishka1@gmail.com>
2021-05-28 11:36:34 +0300

    setup ajax

--- src/reports_proj/urls.py
+++ src/reports_proj/urls.py
@@ -20,7 +20,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('sales.urls', namespace='sales'))
+    path('', include('sales.urls', namespace='sales')),
+    path('reports/', include('reports.urls', namespace='reports'))
+
 ]
 urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- src/sales/static/sales/js/home.js
+++ src/sales/static/sales/js/home.js
@@ -5,6 +5,14 @@ const reportBtn = document.getElementById('report-btn')
 const img =  document.getElementById('img')
 const modalBody =  document.getElementById('modal-body')
 
+const reportName = document.getElementById('id_name')
+const reportRemarks = document.getElementById('id_remarks')
+const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value
+const reportForm = document.getElementById('report-form')
+console.log(reportName)
+console.log(reportRemarks)
+
+
 
 console.log(reportBtn)
 console.log(img)
@@ -17,4 +25,32 @@ reportBtn.addEventListener('click',()=>{
     console.log('clicked')
     img.setAttribute('class', 'w-100')
     modalBody.prepend(img)
+
+//    console.log(img.src)
+    reportForm.addEventListener('submit', e=>{
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('csrfmiddlewaretoken', csrf)
+        formData.append('name',reportName.value)
+        formData.append('remarks', reportRemarks.value)
+        formData.append('image', img.src);
+
+
+        $.ajax({
+            type: 'POST',
+            url: '/reports/save/',
+            data: formData,
+            success: function(response){
+                console.log(response)
+            },
+            error: function(error){
+                console.log(error)
+            },
+            processData: false,
+            contentType: false,
+
+
+        })
+    })
+
 })
