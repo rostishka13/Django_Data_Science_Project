b503b15e1f03fdd5969f13ec88290c02550f67ce
Rostyk <rostishka1@gmail.com>
2021-05-29 07:21:04 +0300

    pdf reports

--- src/reports/urls.py
+++ src/reports/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import create_report_view, ReportListView, ReportDetailView
+from .views import create_report_view, ReportListView, ReportDetailView, render_pdf_view
 
 
 app_name = 'reports'
 
 urlpatterns = [
     path('save/', create_report_view, name='create-report'),
-    path('', ReportListView.as_view(), name = 'main'),
-    path('<pk>', ReportDetailView.as_view(), name = 'detail')
+    path('', ReportListView.as_view(), name='main'),
+    path('<pk>', ReportDetailView.as_view(), name='detail'),
+    path('<pk>/pdf/', render_pdf_view, name='pdf')
 
 ]
--- src/reports/views.py
+++ src/reports/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from profiles.models import Profile
 from django.http import JsonResponse
 from .utils import get_report_image
@@ -6,6 +6,11 @@ from .models import Report
 # Create your views here.
 from .forms import ReportForm
 from django.views.generic import ListView, DetailView
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import get_template
+from xhtml2pdf import pisa
 
 
 
@@ -40,3 +45,26 @@ def create_report_view(request):
         return JsonResponse({'msg': 'send'})
     return JsonResponse({})
 
+
+def render_pdf_view(request, pk):
+    template_path = 'reports/pdf.html'
+    obj = get_object_or_404(Report, pk = pk)
+    context = {'obj': obj}
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    #if download
+    # response['Content-Disposition'] = 'attachment; filename="report.pdf"'
+    #if show in browser
+    response['Content-Disposition'] =  'filename="report.pdf"'
+
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
